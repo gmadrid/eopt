85948efe805955fd2d4954b58e56af453df238b5
George Madrid <gmadrid@gmail.com>
2024-09-17 22:01:33 -0400

    sorting by product now works better and faster.

--- lib/combine.ts
+++ lib/combine.ts
@@ -1,12 +1,11 @@
 import {Product, Transaction} from "@/lib/etradeclient";
-import {formatProduct} from "@/lib/format";
 
 export interface CombinedTransaction {
     combined: boolean | undefined,
 }
 
 // Combine transactions that are on the same day and for the same product.
-// As a side-effect, sorts the transactions by date and product.
+// As a side effect, sorts the transactions by date and product.
 // Adds a 'combined' field to the transactions that were combined.
 const combineTransactions = (txns: Transaction[]): (Transaction & CombinedTransaction)[] => {
     let combined: Transaction[] = [];
@@ -18,26 +17,14 @@ const combineTransactions = (txns: Transaction[]): (Transaction & CombinedTransa
             return -dateCmp;
         }
 
-        const t1product = formatProduct(t1.brokerage.product);
-        const t2product = formatProduct(t2.brokerage.product);
-        if (t1product === undefined && t2product === undefined) {
-            return 0;
-        }
-        if (t1product === undefined) {
-            return -1;
-        }
-        if (t2product === undefined) {
-            return 1;
-        }
-        return t1product.localeCompare(t2product);
+        return compareProducts(t1.brokerage.product, t2.brokerage.product);
     });
 
     let lastTxn: (Transaction | undefined) = undefined;
     txns.forEach(txn => {
         if (lastTxn &&
             lastTxn.transactionDate === txn.transactionDate &&
-            lastTxn.brokerage?.product &&
-            compareProduct(lastTxn.brokerage.product, txn.brokerage.product)) {
+            productsEqual(lastTxn.brokerage?.product, txn.brokerage?.product)) {
             lastTxn.amount += txn.amount;
             lastTxn.brokerage.quantity += txn.brokerage.quantity;
             (lastTxn as Transaction & CombinedTransaction).combined = true;
@@ -49,7 +36,53 @@ const combineTransactions = (txns: Transaction[]): (Transaction & CombinedTransa
     return combined as (Transaction & CombinedTransaction)[];
 }
 
-const compareProduct = (lp?: Product, rp?: Product): boolean =>
+const compareProducts = (lhs?: Product, rhs?: Product): number => {
+    if (lhs == rhs) {
+        return 0;
+    }
+    if (lhs == undefined) {
+        return -1;
+    }
+    if (rhs == undefined) {
+        return 1;
+    }
+    let cmp = compareNullishStrings(lhs.symbol, rhs.symbol);
+    if (cmp !== 0) {
+        return cmp;
+    }
+
+    cmp = (lhs.expiryYear ?? 0) - (rhs.expiryYear ?? 0);
+    if (cmp !== 0) {
+        return cmp;
+    }
+    cmp = (lhs.expiryMonth ?? -1) - (rhs.expiryMonth ?? -1);
+    if (cmp !== 0) {
+        return cmp;
+    }
+    cmp = (lhs.expiryDay ?? -1) - (rhs.expiryDay ?? -1);
+    if (cmp !== 0) {
+        return cmp;
+    }
+
+    cmp = compareNullishStrings(lhs.securityType, rhs.securityType);
+    if (cmp !== 0) {
+        return cmp;
+    }
+
+    cmp = compareNullishStrings(lhs.callPut, rhs.callPut);
+    if (cmp !== 0) {
+        return cmp;
+    }
+
+    cmp = (lhs.strikePrice ?? 0) - (rhs.strikePrice ?? 0);
+    return cmp;
+}
+
+const compareNullishStrings = (lhs?: string, rhs?: string): number => {
+    return (lhs ?? '').localeCompare((rhs ?? ''));
+}
+
+const productsEqual = (lp?: Product, rp?: Product): boolean =>
     lp?.expiryDay === rp?.expiryDay &&
     lp?.expiryMonth === rp?.expiryMonth &&
     lp?.expiryYear === rp?.expiryYear &&
